48ae8fa489fd481b3b876753da821fc65714e8db
HacheGeGit <92006380+HacheGeGit@users.noreply.github.com>
2023-06-23 08:32:17 +0200

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,20 +3,20 @@
 from odoo import http
 
 
-class Biblioteca(http.Controller):
-    @http.route('/biblioteca/biblioteca', auth='public')
+class Farmacia(http.Controller):
+    @http.route('/farmacia/farmacia', auth='public')
     def index(self, **kw):
         return "Hello, world"
 
-    @http.route('/biblioteca/biblioteca/objects', auth='public')
+    @http.route('/farmacia/farmacia/objects', auth='public')
     def list(self, **kw):
-        return http.request.render('biblioteca.listing', {
-            'root': '/biblioteca/biblioteca',
-            'objects': http.request.env['biblioteca.biblioteca'].search([]),
+        return http.request.render('farmacia.listing', {
+            'root': '/farmacia/farmacia',
+            'objects': http.request.env['farmacia.farmacia'].search([]),
         })
 
-    @http.route('/biblioteca/biblioteca/objects/<model("biblioteca.biblioteca"):obj>', auth='public')
+    @http.route('/farmacia/farmacia/objects/<model("farmacia.farmacia"):obj>', auth='public')
     def object(self, obj, **kw):
-        return http.request.render('biblioteca.object', {
+        return http.request.render('farmacia.object', {
             'object': obj
         })
